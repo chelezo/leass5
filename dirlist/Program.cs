e8d0fe5d72107411d75534a0175930bdfd8cf4ef
chelezo <alex.chelezo@gmail.com>
2022-01-17 13:26:27 +0200

    leas5

---
+++ dirlist/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+namespace dirlist
+
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intClass intC = new intClass();
+            intC.TxTHello();
+            intC.DirSearch = Console.ReadLine();
+            intC.FileSearchR();
+            intC.FileRekurs = Console.ReadLine();
+            intC.Catalogi();
+             string[] dirVis = Directory.GetDirectories(intC.DirSearch);
+             string[] fileVis = Directory.GetFiles(intC.DirSearch, intC.FileRekurs);
+           
+            string saveD = JsonConvert.SerializeObject((dirVis, fileVis));
+            File.WriteAllText("Direc.json", saveD);
+            Program loadJsonD = JsonConvert.DeserializeObject<Program>(saveD);
+            System.Console.Write(saveD);
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dirlist/intClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace dirlist
+{
+
+    class intClass
+    {
+        private string _DirSearch;
+        private string _FileRekurs;
+        
+
+
+       
+         
+        public void Catalogi()
+        {
+            System.Console.WriteLine("Каталоги и файлы:");
+        }
+        public void TxTHello()
+        {
+            System.Console.WriteLine("Приветсвуем Вас. Ведите путь к папке напрмер С:\\\\");
+        }
+        public void FileSearchR()
+        {
+            System.Console.WriteLine("Для получения заданных типов файлов укажите тип например *.txt");
+        }
+        public string DirSearch
+       {
+           get { return _DirSearch; }
+           set { _DirSearch = value; }
+        }
+
+        public string FileRekurs
+        {
+            get { return _FileRekurs; }
+            set { _FileRekurs = value; }
+        }
+    }
+}
